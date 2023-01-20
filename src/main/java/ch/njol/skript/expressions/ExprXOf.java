2d3c16fe9a1ac5a53e33dd5bef48638dd13d8109
DelayedGaming <72163224+DelayedGaming@users.noreply.github.com>
2023-01-20 13:26:04 +0800

    Fix #3

--- src/main/java/ch/njol/skript/expressions/ExprXOf.java
+++ src/main/java/ch/njol/skript/expressions/ExprXOf.java
@@ -30,9 +30,9 @@ import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.util.slot.Slot
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
-import ch.njol.util.slot.Slot;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemStack;
 import org.eclipse.jdt.annotation.Nullable;
